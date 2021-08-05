80d6cabd8b3060eebe4556b8e811f66d78ef7eb1
ibanunmangun <62372980+ibanunmangun@users.noreply.github.com>
2021-08-05 18:19:29 +0800

    Add files via upload

---
+++ data/balipointofinterest_2.js
@@ -0,0 +1,23 @@
+var json_balipointofinterest_2 = {
+"type": "FeatureCollection",
+"name": "balipointofinterest_2",
+"crs": { "type": "name", "properties": { "name": "urn:ogc:def:crs:OGC:1.3:CRS84" } },
+"features": [
+{ "type": "Feature", "properties": { "name": "Warung Wardani - Yudistira", "desc": "Warung Wardani - Yudistira, Denpasar, Jl. Yudistira No.2, Dangin Puri Kauh, North Denpasar, Denpasar City, Bali 80232, Indonesia", "X": null, "Y": null, "info": "point of interest" }, "geometry": { "type": "Point", "coordinates": [ 115.2158839, -8.6510946 ] } },
+{ "type": "Feature", "properties": { "name": "Bhineka Jaya (Kopi Bali)", "desc": "Bhineka Jaya (Kopi Bali), Jl. Gajah Mada No.80, Dauh Puri Kaja, North Denpasar, Denpasar City, Bali 80111, Indonesia", "X": null, "Y": null, "info": "point of interest" }, "geometry": { "type": "Point", "coordinates": [ 115.2134019, -8.6553805 ] } },
+{ "type": "Feature", "properties": { "name": "Werdhi Budaya Art Centre", "desc": "Werdhi Budaya Art Centre, Jl. Nusa Indah No.1, Panjer, Denpasar Selatan, Denpasar City, Bali 80236, Indonesia", "X": null, "Y": null, "info": "point of interest" }, "geometry": { "type": "Point", "coordinates": [ 115.2341178, -8.6556059 ] } },
+{ "type": "Feature", "properties": { "name": "Ayam Betutu Khas Gilimanuk", "desc": "Ayam Betutu Khas Gilimanuk, Jl. Merdeka No.88 No.5, Sumerta Kelod, East Denpasar, Denpasar City, Bali 80239, Indonesia", "X": null, "Y": null, "info": "point of interest" }, "geometry": { "type": "Point", "coordinates": [ 115.2383899, -8.6665461 ] } },
+{ "type": "Feature", "properties": { "name": "Gula Bali The Joglo", "desc": "Gula Bali The Joglo, Jl. Merdeka II No.16, Sumerta Kelod, East Denpasar, Denpasar City, Bali 80239, Indonesia", "X": null, "Y": null, "info": "point of interest" }, "geometry": { "type": "Point", "coordinates": [ 115.2400464, -8.6666509 ] } },
+{ "type": "Feature", "properties": { "name": "Warung Mak Beng", "desc": "Warung Mak Beng, Jalan Hang Tuah No.45, Sanur Kaja, Denpasar Selatan, Sanur Kaja, Denpasar Selatan, Kota Denpasar, Bali 80227, Indonesia", "X": null, "Y": null, "info": "point of interest" }, "geometry": { "type": "Point", "coordinates": [ 115.2630096, -8.6736865 ] } },
+{ "type": "Feature", "properties": { "name": "Rumah Sakit Bhayangkara Denpasar", "desc": null, "X": null, "Y": null, "info": "public facilities" }, "geometry": { "type": "Point", "coordinates": [ 115.224532, -8.645987 ] } },
+{ "type": "Feature", "properties": { "name": "Bali Royal Hospital", "desc": null, "X": null, "Y": null, "info": "public facilities" }, "geometry": { "type": "Point", "coordinates": [ 115.227706, -8.668345 ] } },
+{ "type": "Feature", "properties": { "name": "Kepolisian Daerah Bali", "desc": null, "X": null, "Y": null, "info": "public facilities" }, "geometry": { "type": "Point", "coordinates": [ 115.224825, -8.648194 ] } },
+{ "type": "Feature", "properties": { "name": "Polsek Denpasar Selatan", "desc": null, "X": null, "Y": null, "info": "public facilities" }, "geometry": { "type": "Point", "coordinates": [ 115.259482, -8.67652 ] } },
+{ "type": "Feature", "properties": { "name": "SPBU Pertamina 54.801.10", "desc": null, "X": null, "Y": null, "info": "public facilities" }, "geometry": { "type": "Point", "coordinates": [ 115.209362, -8.644696 ] } },
+{ "type": "Feature", "properties": { "name": "Pertamina Kreneng", "desc": null, "X": null, "Y": null, "info": "public facilities" }, "geometry": { "type": "Point", "coordinates": [ 115.225243, -8.656651 ] } },
+{ "type": "Feature", "properties": { "name": "Masjid Al-Amanah", "desc": null, "X": null, "Y": null, "info": "public facilities" }, "geometry": { "type": "Point", "coordinates": [ 115.231711, -8.671368 ] } },
+{ "type": "Feature", "properties": { "name": "Saint Joseph Catholic Church", "desc": null, "X": null, "Y": null, "info": "public facilities" }, "geometry": { "type": "Point", "coordinates": [ 115.22022, -8.65591 ] } },
+{ "type": "Feature", "properties": { "name": "Pura Agung Jagatnatha", "desc": null, "X": null, "Y": null, "info": "public facilities" }, "geometry": { "type": "Point", "coordinates": [ 115.218815, -8.656566 ] } },
+{ "type": "Feature", "properties": { "name": "Vihara Vimala Maitreya", "desc": null, "X": null, "Y": null, "info": "public facilities" }, "geometry": { "type": "Point", "coordinates": [ 115.2112, -8.653747 ] } }
+]
+}
